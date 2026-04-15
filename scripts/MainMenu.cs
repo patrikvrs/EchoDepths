1fe1b599b8b7b1bf14525bd0eae562b06388572f
patrikvrs <143280004+patrikvrs@users.noreply.github.com>
2026-04-15 12:06:21 +0200

    Added options menu; Added PublicPixel font

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -4,15 +4,28 @@ public partial class MainMenu : Control
 {
     [Export]
     private PackedScene _gameScene;
-    [Export]
     private Button _startButton;
-    [Export]
+    private Button _optionsButton;
     private Button _quitButton;
+    private BoxContainer _mainButtonsContainer;
+    private Button _backButton;
+    private Panel _optionsPanel;
 
     public override void _Ready()
     {
+        _startButton = GetNode<Button>("MainMenuButtons/StartButton");
+        _optionsButton = GetNode<Button>("MainMenuButtons/OptionsButton");
+        _quitButton = GetNode<Button>("MainMenuButtons/QuitButton");
+        _backButton = GetNode<Button>("OptionsPanel/BackButton");
+        _mainButtonsContainer = GetNode<BoxContainer>("MainMenuButtons");
+        _optionsPanel = GetNode<Panel>("OptionsPanel");
+
         _startButton.Pressed += OnStartButtonPressed;
+        _optionsButton.Pressed += OnOptionsButtonPressed;
         _quitButton.Pressed += OnQuitButtonPressed;
+        _backButton.Pressed += OnOptionsButtonPressed;
+
+        _optionsPanel.Visible = false;
     }
 
     private void OnStartButtonPressed()
@@ -25,6 +38,12 @@ public partial class MainMenu : Control
         {
             GD.PrintErr("Game scene is not assigned.");
         }
+    }
+
+    private void OnOptionsButtonPressed()
+    {
+        _optionsPanel.Visible = !_optionsPanel.Visible;
+        _mainButtonsContainer.Visible = !_mainButtonsContainer.Visible;
     }
 
     private void OnQuitButtonPressed()
